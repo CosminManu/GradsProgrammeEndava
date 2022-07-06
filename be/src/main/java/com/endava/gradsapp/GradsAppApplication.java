6a3b57fff7cb2ea90aca54a6c1eb3fadff885751
CosminManu <cosmin.manu2000@gmail.com>
2022-07-06 12:57:25 +0300

    init

---
+++ be/src/main/java/com/endava/gradsapp/GradsAppApplication.java
@@ -0,0 +1,13 @@
+package com.endava.gradsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradsAppApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/com/endava/gradsapp/GradsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.gradsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
