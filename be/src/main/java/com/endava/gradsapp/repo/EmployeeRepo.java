a902e8f632f1d6693af63000f2bfb3d9f59f837a
CosminManu <cosmin.manu2000@gmail.com>
2022-07-26 13:35:34 +0300

    work ahead: service, exceptions, controller

--- be/src/main/java/com/endava/gradsapp/repo/EmployeeRepo.java
+++ be/src/main/java/com/endava/gradsapp/repo/EmployeeRepo.java
@@ -2,8 +2,14 @@ package com.endava.gradsapp.repo;
 
 import com.endava.gradsapp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-    void deleteEmployeesById(Long id);
-    Employee findEmployeesById(Long id);
+    @Query("DELETE from Employee WHERE id = :id")
+    void deleteEmployeeById(Long id);
+
+    @Query("SELECT a from Employee a WHERE  a.id = :id")
+    Optional<Employee> findEmployeesById(Long id);
 }
