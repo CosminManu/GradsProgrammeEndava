c69c4a4f7bdac4286a6a3849d206a884a8fe5ecb
CosminManu <cosmin.manu2000@gmail.com>
2022-07-26 21:11:55 +0300

    modified controller

--- be/src/main/java/com/endava/gradsapp/controller/EmployeeController.java
+++ be/src/main/java/com/endava/gradsapp/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.endava.gradsapp.model.Employee;
 import com.endava.gradsapp.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,6 +31,23 @@ public class EmployeeController {
         return new ResponseEntity<>(found, HttpStatus.OK);
     }
 
+    @PostMapping("/new")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/change")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        Employee updatedEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 
 }
